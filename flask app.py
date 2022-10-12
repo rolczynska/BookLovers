5e90f6f020de6a7a6c8c124403c0d240a61e0ec0
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-10-12 17:10:01 +0200

    Write route 'answer' Write route/answer/enter_email

--- flask app.py
+++ flask app.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template, request
-
+from address import get_url, search_for_book_status, send_mail
 app = Flask(__name__)
 
+
+searching_books = []
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/answer")
+def answer():
+    title = request.args.get("title")
+    if not title:
+        return render_template("failure.html")
+    url = get_url(title)
+    availability = search_for_book_status(url)
+    if availability:
+        return render_template("book_available.html")
+    searching_books.append(title)
+    return render_template("enter_email.html")
+
+
+@app.route("/answer/enter_email", methods=["POST"])
+def enter_email():
+    email = request.form.get("email")
+    send_mail(email, searching_books[-1])
+    return render_template("email_registered.html")
 
 
 if __name__ == '__main__':
