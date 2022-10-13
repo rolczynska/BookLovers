5a5ec30380595f121e3c63d056ebdb6bfa5214c8
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-10-13 17:00:26 +0200

    Changes in flask_app.py

--- flask app.py
+++ flask app.py
@@ -1,34 +1,45 @@
 from flask import Flask, render_template, request
-from address import get_url, search_for_book_status, send_mail
+
+import main
+from address import get_url, check_for_book_status
+from mail import send_mail, send_register_confirmation
+from tools import add_to_list, is_already_registered, searching_titles
+
 app = Flask(__name__)
 
-
-searching_books = []
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# TODO czy ta opcja z global jest poprawna?
 @app.route("/answer")
 def answer():
+    global title
     title = request.args.get("title")
     if not title:
         return render_template("failure.html")
     url = get_url(title)
-    availability = search_for_book_status(url)
+    availability = check_for_book_status(url)
     if availability:
         return render_template("book_available.html")
-    searching_books.append(title)
     return render_template("enter_email.html")
 
 
-@app.route("/answer/enter_email", methods=["POST"])
+# TODO zrób template "already_registered.html
+@app.route("/enter_email", methods=["POST"])
 def enter_email():
     email = request.form.get("email")
-    send_mail(email, searching_books[-1])
-    return render_template("email_registered.html")
+    if is_already_registered(title, email):
+        return render_template("already_registered.html")
+    else:
+        add_to_list(title, email)
+        send_register_confirmation(title, email)
+        return render_template("email_registered.html")
 
 
 if __name__ == '__main__':
     app.run()
+    main.searching_books()
+
