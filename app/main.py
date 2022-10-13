1d796f349a76d13bfb8acc3e4bcfcc2ce3ad90e9
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-10-13 17:05:27 +0200

    Write two main stages of program. first_check() and searching_books()

--- app/main.py
+++ app/main.py
@@ -1,71 +1,44 @@
-import requests
-import yagmail
+from app import address, tools, mail
 import time
-from bs4 import BeautifulSoup
-from typing import List, Dict
-from datetime import datetime
-from app import address
 
 
-def main():
-    """Main stages of program."""
+def first_check():
+    """ Stages when user enter a title in applicati"""
+    # program ma się odpalać gdy ktoś wyśle zapytanie za pomocą formularza - przycisk submit
+    # we get the title.
+    title = ...
+    while title:
+        # convert it into SINGLE url.
+        url = address.get_url(title)
+        # checking status for book
+        availability = address.check_for_book_status(url)
+        if not availability:
+            # if it is not availabile ask about mail
+            email = ...
+            # sprawdzamy czy tytuł z tym mailem jest dodany do listy sprawdzającej
+            if not tools.is_already_registered(title, email):
+                # jeśli nie jest zarejestrwana to wysyłamy potwierdzenie i dodajemy tytuł i email do listy
+                mail.send_register_confirmation(title, email)
+                tools.add_to_list(title, email)
+            else:
+                return "You are already registered for that book."
+        return "Your book is already available!"
+
+
+def searching_books():
     while True:
-        # somehow we need to get the title.
-        title = ...
-        # and then convert it into SINGLE url.
-        urls = address.get_url(title)
-        books_status = search_for_status(urls)
-        available_books = check_is_it_available(books_status)
-        if available_books:
-            send_mail(available_books)
-        else:
-            print("Books you're searching for are unavailable.")
+        for title, email in tools.searching_titles.items():
+            # convert it into SINGLE url.
+            url = address.get_url(title)
+            # checking status for book
+            availability = address.check_for_book_status(url)
+            # if it is available, send mail and delete from searching_titles.
+            if availability:
+                mail.send_mail(title, email)
+                tools.searching_titles.pop(title)
         print("Sleeping...")
         time.sleep(60 * 60 * 12)
 
 
-def search_for_status(urls: Dict[str, str]) -> Dict[str, list]:
-    """Function takes a dict {title: url,...} and return results {title:[status, status]} from the website."""
-    results = {}
-    for title, url in urls.items():
-        result = requests.get(url)
-        html = BeautifulSoup(result.text, "html.parser")
-        libraries = html.find_all(["tr"], height="15")
-        status_for_all_copies = []
-        for tag in libraries:
-            all_columns = tag.find_all("td")
-            first_column = tag.find("td")
-            name = first_column.string
-            if name == "Wypożyczalnia Al. Marcinkowskiego 23":
-                status = all_columns[5].string
-                status_for_all_copies.append(status)
-        results[title] = status_for_all_copies
-    return results
-
-
-def check_is_it_available(titles_availability: Dict[str, list]) -> List[str]:
-    """ Function take a titles and list of statuses and return only available titles."""
-    available_books = []
-    for title, availability in titles_availability.items():
-        if "Na półce" in availability:
-            available_books.append(title)
-    return available_books
-
-
-def send_mail(available_books: List[str]):
-    """ Function take a list of available books titles and send a mail."""
-    mail_from = 'olkiewicz.alex1234@gmail.com'
-    mail_to = 'olkiewicz.alex@gmail.com'
-    subject = 'Dostępne książki'
-    sender_password = 'epszxtotnzklwwhb'
-    yag = yagmail.SMTP(user=mail_from, password=sender_password)
-    books = ""
-    for book in available_books:
-        books += book + " "
-    contents = 'Dostępne książki:\n' + books
-    yag.send(to=mail_to, subject=subject, contents=contents)
-    print(f'Mail sent at {datetime.now() :%d-%m-%Y %H:%M}.')
-
-
 if __name__ == '__main__':
-    main()
+    searching_books()
