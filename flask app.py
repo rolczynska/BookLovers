927647db205257c97544432a99d3431d92860d66
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-10-14 15:15:59 +0200

    Solution to pass title to another route in flask_app.py

--- flask app.py
+++ flask app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 
-import main
+from main import search_books
 from address import get_url, check_for_book_status
 from mail import send_mail, send_register_confirmation
-from tools import add_to_list, is_already_registered, searching_titles
+from tools import add_to_list, is_already_registered, decode, HOME
 
 app = Flask(__name__)
 
@@ -16,10 +16,7 @@ def index():
 # TODO czy ta opcja z global jest poprawna?
 @app.route("/answer")
 def answer():
-    global title
     title = request.args.get("title")
-    if not title:
-        return render_template("failure.html")
     url = get_url(title)
     availability = check_for_book_status(url)
     if availability:
@@ -27,19 +24,23 @@ def answer():
     return render_template("enter_email.html")
 
 
-# TODO zrób template "already_registered.html
+# TODO zrób template "already_registered.html"
 @app.route("/enter_email", methods=["POST"])
 def enter_email():
+    url_title = request.referrer
     email = request.form.get("email")
-    if is_already_registered(title, email):
+    title = decode(url_title)
+    if is_already_registered(title, email, path=HOME / "searching_books.json"):
         return render_template("already_registered.html")
     else:
-        add_to_list(title, email)
+        add_to_list(title, email, path=HOME / "searching_books.json")
         send_register_confirmation(title, email)
         return render_template("email_registered.html")
+    # requests.post('http://127.0.0.1:5001/ ', data={'key': 'value'})
+
+    # postawić drugą aplikacje która obsługuje szukanie po liście tytułów i wysyła maila.
+    # Ma to być w oddzielnym folderze.
 
 
 if __name__ == '__main__':
     app.run()
-    main.searching_books()
-
