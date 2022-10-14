045b45f4e9f9960243521d80a6c866c2ba75af16
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-10-14 15:14:08 +0200

    Write send_register_confirmation() in mail.py

--- app/mail.py
+++ app/mail.py
@@ -3,7 +3,14 @@ from datetime import datetime
 
 
 def send_register_confirmation(title, email):
-    pass
+    mail_from = 'olkiewicz.alex1234@gmail.com'
+    mail_to = email
+    subject = 'Dostępne książki'
+    sender_password = 'epszxtotnzklwwhb'
+    yag = yagmail.SMTP(user=mail_from, password=sender_password)
+    contents = f'You are registered for notification in Raczyński library. When the "{title}" book will be available we will send you an e-mail.'
+    yag.send(to=mail_to, subject=subject, contents=contents)
+    print(f'Mail sent to {email} at {datetime.now() :%d-%m-%Y %H:%M}.')
 
 
 def send_mail(title, email):
