ace9576fb04a90fb6bb0eb9b769e37fece49410e
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-10-16 16:15:26 +0200

    Add def confirm_title_author in address.py

--- app/address.py
+++ app/address.py
@@ -5,19 +5,18 @@ from datetime import datetime
 # https://br-hip.pfsl.poznan.pl/ipac20/ipac.jsp?session=166B5852R74W9.199857&amp;profile=br-mar&amp;uri=link=3100033~!2651526~!3100021~!3100029&amp;aspect=basic_search&amp;menu=search&amp;ri=1&amp;source=~!bracz&amp;term=365+dni+%2F&amp;index=ALTITLE
 
 
-# TODO sprawdzić czy to na pewno ten tytuł i autor, sprawdzić czy nie ma dwóch pozycji tego samego tytułu (inne wydanie)
-def get_url(title: str):
-    """ Function take a title and return an url where information about book status is display."""
+# TODO sprawdzić czy nie ma dwóch pozycji tego samego tytułu (inne wydanie)
+def confirm_title_author(title: str):
+    """ Function take a title and check is this book in library.
+     Return an url where information about book status is display."""
     replaced_title = title.replace(" ", "+")
     url_formula = f'https://br-hip.pfsl.poznan.pl/ipac20/ipac.jsp?index=ALTITLE&term={replaced_title}'
     page = requests.get(url_formula)
     parsed_page = BeautifulSoup(page.text, "html.parser")
-    tags = parsed_page.find(class_="smallBoldAnchor", href=True)
-    web_title = tags.text
-    if web_title == title:
-        url = tags.get("href")
-        return url
-    return False
+    tags = parsed_page.find_all(class_="smallBoldAnchor")
+    web_title = tags[0].text
+    web_author = tags[1].text
+    return web_title, web_author
 
 
 # TODO Co jeśli nie ma książki ? - zaimplementuj odpowiedź
@@ -38,3 +37,19 @@ def check_for_book_status(url) -> bool:
         return True
     else:
         return False
+
+
+def get_url(title):
+    replaced_title = title.replace(" ", "+")
+    url_formula = f'https://br-hip.pfsl.poznan.pl/ipac20/ipac.jsp?index=ALTITLE&term={replaced_title}'
+    page = requests.get(url_formula)
+    parsed_page = BeautifulSoup(page.text, "html.parser")
+    tags = parsed_page.find_all(class_="smallBoldAnchor")
+    url = tags[0].get("href")
+    return url
+
+
+  #if web_title == title or title in web_title:
+   #     if is_this_book(web_title, web_author, tags):
+    #        url = tags.get("href")
+     #       return url
--- tests/test_address.py
+++ tests/test_address.py
@@ -3,5 +3,5 @@ import pytest
 
 
 def test_get_url():
-    result = address.get_url("365 dni")
+    result = address.confirm_title_author("365 dni")
     assert result == "https://br-hip.pfsl.poznan.pl/ipac20/ipac.jsp?session=166B5852R74W9.199857&amp;profile=br-mar&amp;uri=link=3100033~!2651526~!3100021~!3100029&amp;aspect=basic_search&amp;menu=search&amp;ri=1&amp;source=~!bracz&amp;term=365+dni+%2F&amp;index=ALTITLE"
