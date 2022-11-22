a5836c7be31107f28ca13cbbb88e34afeeac29ef
aleksandra-olkiewicz <olkiewicz.alex@gmail.com>
2022-11-22 11:55:02 +0100

    Change mail.py - adding mail content and logo image.

--- app/mail.py
+++ app/mail.py
@@ -1,17 +1,21 @@
 import yagmail
 from datetime import datetime
+from tools import HOME
+TEMPLATES = HOME / '..' / 'templates'
+STATIC = HOME / '..' / 'static'
 
 
 def send_register_confirmation(title, email):
+    """Function send a confirmation after registration for notification."""
     mail_from = 'olkiewicz.alex1234@gmail.com'
     mail_to = email
     subject = 'Dostępne książki'
     sender_password = 'epszxtotnzklwwhb'
     yag = yagmail.SMTP(user=mail_from, password=sender_password)
-    message = f'You are registered for notification in Raczyński library. When the "{title}" book will be available we will send you an e-mail.\n'
-    cancel_notification = f"If you want to cancel your subscription for this book please click on <a href='http://127.0.0.1:5000/cancel_notify/{title}/{email}'>THIS LINK.</a>"
-    contents = message + cancel_notification
-    yag.send(to=mail_to, subject=subject, contents=contents)
+    with open(TEMPLATES / 'mail_content.html', 'r') as file:
+        content = file.read()
+        contents = [yagmail.inline(STATIC / 'logo3b.png'), content.format(title=title, email=email)]
+        yag.send(to=mail_to, subject=subject, contents=contents)
     print(f'Mail sent to {email} at {datetime.now() :%d-%m-%Y %H:%M}.')
 
 
@@ -22,6 +26,8 @@ def send_mail(title, email):
     subject = 'Dostępne książki'
     sender_password = 'epszxtotnzklwwhb'
     yag = yagmail.SMTP(user=mail_from, password=sender_password)
-    contents = f'Huuray! "{title}" is already available in Raczyński library. Go and borrow it!'
+    with open(TEMPLATES / 'mail_book_available.html', 'r') as file:
+        content = file.read()
+    contents = content.format(title=title)
     yag.send(to=mail_to, subject=subject, contents=contents)
     print(f'Mail sent to {email} at {datetime.now() :%d-%m-%Y %H:%M}.')
